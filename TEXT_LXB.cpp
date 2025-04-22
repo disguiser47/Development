0546137c1feb43bca1b15644680e531dcb6b9adb
disguiser47 <1521504108@qq.com>
2025-04-22 14:36:52 +0800

    master commit 1

---
+++ TEXT_LXB.cpp
@@ -0,0 +1,34 @@
+﻿// TEXT_LXB.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "Development.h"
+#include "afxdialogex.h"
+#include "TEXT_LXB.h"
+
+
+// TEXT_LXB 对话框
+
+IMPLEMENT_DYNAMIC(TEXT_LXB, CDialogEx)
+
+TEXT_LXB::TEXT_LXB(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG_LXB, pParent)
+{
+
+}
+
+TEXT_LXB::~TEXT_LXB()
+{
+}
+
+void TEXT_LXB::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(TEXT_LXB, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// TEXT_LXB 消息处理程序
---
+++ TEXT_LXB.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// TEXT_LXB 对话框
+
+class TEXT_LXB : public CDialogEx
+{
+	DECLARE_DYNAMIC(TEXT_LXB)
+
+public:
+	TEXT_LXB(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~TEXT_LXB();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG_LXB };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
